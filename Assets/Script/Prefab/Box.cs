f46c005dfe6903b332e7bbdf24cea1918677f140
lvboluo24 <1259918933@qq.com>
2026-05-18 23:01:55 +0800

    箱子瞬移逻辑

--- Assets/Script/Prefab/Box.cs
+++ Assets/Script/Prefab/Box.cs
@@ -25,6 +25,8 @@ public class Box : MonoBehaviour
 
     [Tooltip("0，两世界显示，1，和平显示，战争隐藏，2，和平隐藏，战争显示")]
     public int display;
+    [Tooltip("是否传送回初始点")]
+    public bool _isReturn;
     private List<Vector3> PathNodes = new List<Vector3>();//记录箱子路径点位置
 
     private Game game;
@@ -59,22 +61,42 @@ public class Box : MonoBehaviour
         //到达目标点逻辑
         if (Vector3.Distance(transform.position, PathNodes[index]) < 0.1f)
         {
-            //0，自动往返
-            if (type == 0)
+            Round();
+
+        }
+        UpdateDisplay();
+    }
+    public void UpdateWorld()
+    {
+        Round();
+    }
+    public void Round()
+    {
+        //往返逻辑
+        //0，自动往返
+        if (type == 0)
+        {
+            if (goal == 0)
             {
-                if (goal == 0)
+                if (index == 0)
                 {
-                    if (index == 0)
-                    {
-                        goal = 1;
-                        index += 1;
-                    }
-                    else
-                    {
-                        index--;
-                    }
+                    goal = 1;
+                    index += 1;
                 }
-                else if (goal == 1)
+                else
+                {
+                    index--;
+                }
+            }
+            else if (goal == 1)
+            {
+                if (_isReturn)
+                {
+                    index = PathNodes.Count - 1;
+                    //直接传送回初始点
+                    transform.position = PathNodes[0];
+                }
+                else
                 {
                     if (index == PathNodes.Count - 1)
                     {
@@ -86,60 +108,82 @@ public class Box : MonoBehaviour
                         index++;
                     }
                 }
+
             }
-            //1，根据世界移动
-            else if (type == 1)
+        }
+        //1，根据世界移动
+        else if (type == 1)
+        {
+            //如果箱子世界类型为和平前往终点
+            if (worldType == 0)
             {
-                //如果箱子世界类型为和平前往终点
-                if (worldType == 0)
+                //如果为和平世界
+                if (game.worldType == 0)
                 {
-                    //如果为和平世界
-                    if (game.worldType == 0)
+                    //如果未到达终点
+                    if (index < PathNodes.Count - 1)
                     {
-                        //如果未到达终点
-                        if (index < PathNodes.Count - 1)
-                        {
-                           index++;
-                        }
+                        index++;
                     }
-                    //如果为战争世界
-                    else if (game.worldType == 1)
+                }
+                //如果为战争世界
+                else if (game.worldType == 1)
+                {
+                    if (_isReturn)
                     {
-                        //如果未到达初始点
-                        if (index > 0)
-                        {
-                            index--;
-                        }
+                        transform.position = PathNodes[0];
+                        index = 0;
+                    }
+                    else
+                    {
+                    //如果未到达初始点
+                    if (index > 0)
+                    {
+                        index--;
+                    }
+                    }
+                    
+                }
+
+            }
+            //如果为战争前往终点
+            else if (worldType == 1)
+            {
+                //如果为和平世界
+                if (game.worldType == 0)
+                {
+                    if (_isReturn)
+                    {
+                        transform.position = PathNodes[0];
+                        index = 0;
+                    }
+                    else
+                    {
+                                            //如果未到达初始点
+                    if (index > 0)
+                    {
+                        index--;
+                    }
                     }
 
                 }
-                //如果为战争前往终点
-                else if (worldType == 1)
+                //如果为战争世界
+                else if (game.worldType == 1)
                 {
-                    //如果为和平世界
-                    if (game.worldType == 0)
+                    
+                   
+                    
+                    //如果未到达终点
+                    if (index < PathNodes.Count - 1)
                     {
-                        //如果未到达初始点
-                        if (index > 0)
-                        {
-                            index--;
-                        }
+                        index++;
                     }
-                    //如果为战争世界
-                    else if (game.worldType == 1)
-                    {
-                        //如果未到达终点
-                        if (index < PathNodes.Count - 1)
-                        {
-                           index++;
-                        }
-                    }
-                }
+                    
 
+                }
             }
 
         }
-        UpdateDisplay();
     }
     //显示逻辑
     private void UpdateDisplay()
