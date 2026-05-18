c1e7f75b43b80c8ab571e460621293d9bec867d5
lvboluo24 <1259918933@qq.com>
2026-05-19 00:41:37 +0800

    幽灵怪逻辑

---
+++ Assets/Script/Prefab/Ghost.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    [Tooltip("移动速度")]
+    public float moveSpeed;
+    [Tooltip("类型，0，和平追，战争停，1，和平停")]
+    public int type;
+    private Game game;
+    private Player player;
+    public Scope scope;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        game = GameObject.Find("GameManager").GetComponent<Game>();
+        player = GameObject.Find("Player").GetComponent<Player>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        if (scope._isPlayer)
+        {
+            if (type == 0 && game.worldType == 0 || type == 1 && game.worldType == 1)
+            {
+                Vector2 moveDirection = (player.transform.position - transform.position).normalized;
+                rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
+            }
+
+        }
+
+    }
+}
