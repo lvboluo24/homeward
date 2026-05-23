f4f901893e8ffc4f10e5fa37bfa4c9c8ef09c60f
lvboluo24 <1259918933@qq.com>
2026-05-23 21:21:30 +0800

    设置音量大小

--- Assets/Script/UI/Set.cs
+++ Assets/Script/UI/Set.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Set : MonoBehaviour
 {
     [Tooltip("0,设置菜单，1角色键位教学，2，怀表ui教学，3，普通小齿轮教学，4，手雷齿轮，5，地雷齿轮，6，大齿轮")]
     public List<GameObject> node = new List<GameObject>();
+    [Tooltip("音乐大小")]
+    public float musicvolume;
+    [Tooltip("音效大小")]
+    public float soundvolume;
+    public Slider musicSlider;
+    public Slider soundSlider;
     void Awake()
     {
         node[0].SetActive(false);
     }
     void Start()
     {
-        
+        musicSlider.onValueChanged.AddListener(setmusicvolume);
+        soundSlider.onValueChanged.AddListener(setsoundvolume);
     }
 
     // Update is called once per frame
@@ -48,5 +56,15 @@ public class Set : MonoBehaviour
     public void close(int index)
     {
         node[index].SetActive(false);
+    }
+    //调整音乐大小
+    public void setmusicvolume(float volume)
+    {
+        musicvolume = volume;
+    }
+    //调整音效大小
+    public void setsoundvolume(float volume)
+    {
+        soundvolume = volume;
     }
 }
