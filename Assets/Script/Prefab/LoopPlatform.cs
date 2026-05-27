96829fb594c5357b55a3ef603c16f2482604f3a7
lvboluo24 <1259918933@qq.com>
2026-05-27 21:45:12 +0800

    修复bug

--- Assets/Script/Prefab/LoopPlatform.cs
+++ Assets/Script/Prefab/LoopPlatform.cs
@@ -11,10 +11,20 @@ public class LoopPlatform : MonoBehaviour
     public List<GameObject> PathNodes=new List<GameObject>();
     [Tooltip("目标点索引,0,起点，1，终点")]
     public int index = 0;
-        [Tooltip("启动")]
+    [Tooltip("启动")]
     public bool _isStart;
+    [Tooltip("平台类型,2,两世界显示，0，和平世界显示，1，战争世界显示")]
+    public int display;
     //sp图像
     public SpriteRenderer spRenderer;
+    //碰撞体
+    public Collider2D collider1;
+    Game game;
+     void Awake()
+    {
+        game = GameObject.Find("GameManager").GetComponent<Game>();
+
+    }
     void Start()
     {
 
@@ -35,6 +45,38 @@ public class LoopPlatform : MonoBehaviour
             //如果到达目标点,摧毁
             Destroy(gameObject);
 
+        }
+        if (display==0)
+        {
+            //如果为和平世界
+            if (game.worldType == 1)
+            {
+               //隐藏平台
+               spRenderer.enabled = false;
+               //隐藏碰撞体
+               collider1.enabled = false;
+            }
+            else if (game.worldType == 0)
+            {
+               spRenderer.enabled = true;
+               collider1.enabled = true;
+            }
+        }
+        else if (display==1)
+        {
+            //如果为战争世界
+            if (game.worldType == 0)
+            {
+               //隐藏平台
+               spRenderer.enabled = false;
+               //隐藏碰撞体
+               collider1.enabled = false;
+            }
+            else if (game.worldType == 1)
+            {
+               spRenderer.enabled = true;
+               collider1.enabled = true;
+            }
         }
 
     }
